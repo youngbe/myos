9e32af0274dfd81429d5a1755d2b2aa943f0c254
xyb <>
2021-11-11 22:45:38 +0800

    a

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-if [ "$1" == "" ]; then
-    as --64 boot_loader.s -o boot_loader.o
-else
-    as --64 "$1" -o boot_loader.o
-fi
+as --64 boot_loader.s -o boot_loader.o
 ld --oformat binary -Ttext 0x7c00 -e 0x7c00 -o boot_loader.bin boot_loader.o
-> boot.img
-dd if=boot_loader.bin of=boot.img conv=notrunc
-dd if=bzImage of=boot.img seek=2 conv=notrunc
+dd if=boot_loader.bin of=boot.img conv=sync,fdatasync
+dd if=bzImage of=boot.img seek=2 conv=sync,fdatasync
+rm -rf test.vmdk
 qemu-img convert -f raw -O vmdk boot.img test.vmdk
