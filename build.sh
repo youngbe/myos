eb7adc435610ebbe369652ce4a5e914413223211
xyb <>
2021-11-12 15:35:23 +0800

    a

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ as --64 boot_loader.s -o boot_loader.o
 ld --oformat binary -Ttext 0x7c00 -e 0x7c00 -o boot_loader.bin boot_loader.o
 dd if=boot_loader.bin of=boot.img conv=sync,fdatasync
 dd if=bzImage of=boot.img seek=2 conv=sync,fdatasync
-rm -rf test.vmdk
-qemu-img convert -f raw -O vmdk boot.img test.vmdk
+rm -rf boot.vmdk
+qemu-img convert -f raw -O vmdk boot.img boot.vmdk
+echo "==========================="
+echo "生成boot.img，可用于qemu"
+echo "生成boot.vmdk，可用于vmware"
