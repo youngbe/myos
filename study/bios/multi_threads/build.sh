cedfeb58bbb13b08d37d7ae02159ef4b84bf0939
x <X>
2022-02-25 13:17:14 +0800

    x

--- study/bios/multi_threads/build.sh
+++ study/bios/multi_threads/build.sh
@@ -13,9 +13,9 @@ check_dependency()
 }
 check_dependency
 set -e
-as --64 boot_loader.s -o boot_loader.o
-ld --oformat binary -Ttext 0x7c00 -Tbss 0x0 -o boot_loader.bin boot_loader.o
-dd if=boot_loader.bin of=boot.img conv=sync,fdatasync
+as --64 bootloader.s -o bootloader.o
+ld --oformat binary -Ttext 0x7c00 -Tbss 0x0 -o bootloader.bin bootloader.o
+dd if=bootloader.bin of=boot.img conv=sync,fdatasync
 dd if=/dev/zero of=boot.img seek=1 count=4096 conv=sync,fdatasync
 rm -rf boot.vmdk
 qemu-img convert -f raw -O vmdk boot.img boot.vmdk
