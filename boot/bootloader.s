6d3bced2dca58e7afcd8faee24aa5856f4f28727
Your Name <you@example.com>
2022-02-27 15:36:54 +0800

    x

--- boot/bootloader.s
+++ boot/bootloader.s
@@ -13,13 +13,332 @@ _start:
     # 重置 %cs 和 %eip
     # 抄自grub：https://git.savannah.gnu.org/gitweb/?p=grub.git;a=blob;f=grub-core/boot/i386/pc/boot.S#l227
     ljmpl $0, $.Lreal_start
+
+    # 下一个要读取的逻辑扇区号，0号已经被读取到0x7c00
+.Ldata_sector_num:
+    .long 1
+
+    # 内核代码接下来要加载位置
+.Ldata_protected_mode_code:
+    .long 0x100000
+
+    /*
+    // gdt
+    # 八字节对齐也是抄自Linux源代码
+    .balign	8
+    # 抄自Linux源代码，使第一个段描述符(原本应该使八字节全0)，存放gdt
+    # 从而节省空间
+    # https://elixir.bootlin.com/linux/v5.16.11/source/arch/x86/boot/compressed/head_64.S#L701
+    */
+
+    // 上面一段注释还没搞明白，暂时弃用
+    // gdt
+    .balign 8
+.Lgdt_null:
+    .quad 0
+.Lgdt_code64:
+    .long 0
+    .byte 0
+    .byte 0b10011010
+    .byte 0b00100000
+    .byte 0
+.Lgdt_data64:
+    .long 0
+    .byte 0
+    .byte 0b10010010
+    .word 0
+.Lgdt64_end:
+.Lgdt_code32:
+    .word 0xffff
+    .word 0
+    .byte 0
+    .byte 0b10011011
+    .byte 0b11001111
+    .byte 0
+.Lgdt_code16:
+    .word 0xffff
+    .word 0
+    .byte 0
+    .byte 0b10011011
+    .word 0
+.Lgdt_data32:
+    .word 0xffff
+    .word 0
+    .byte 0
+    .byte 0b10010011
+    .byte 0b11001111
+    .byte 0
+.Lgdt_data16:
+    .word 0xffff
+    .word 0
+    .byte 0
+    .byte 0b10010011
+    .word 0
+
+.Lgdt_ptr:
+    .word .Lgdt_ptr - .Lgdt_null -1
+    .long .Lgdt_null
+
 .Lreal_start:
+    xorl    %eax, %eax
+    cli
+    movw    %ax, %ss
+    movl    $.Lstack_end_and_heap_start, %esp
+    movw    %ax, %ds
+    movw    %ax, %es
+    movw    %ax, %fs
+    movw    %ax, %gs
+    sti
+    call    .Lclear
+
+    //检查一个扇区是不是512字节
+    # qemu不支持int $0x13, %ah=$0x48
+    #subw    $0x1e, %sp
+    ##movw    %ss, %ax
+    ##movw    %ax, %ds
+    #movw    %sp, %si
+    #movb    $0x48, %ah
+    #movb    $0x80, %dl
+    #int     $0x13
+    #jc      .Lerror
+    #testb   %ah, %ah
+    #jne     .Lerror
+    #cmpw    $0x200, 24(%esp)
+    #jne     .Lerror
+    #addw    $0x1e, %sp
+    #call    .Lclear
+
+    //读取Bootloader剩余部分
+    # 因为我的Bootloader有点大，512字节装不下，我写了两个扇区
+    // 同时读取内核头扇区
+    movw    $2, %ax
+    movl    $0x7e00000, %edx
+    call    .Lread_hdd
+
+    jmp     .Lpart2
+
+    //参数：读取扇区数量%ax，保存位置segment:offset : %edx
+.Lread_hdd:
+    andl    $0xffff, %eax
+
+    # 部分bios(比如vmware)的拓展读取磁盘服务最大一次只能读127个扇区
+    # https://en.wikipedia.org/wiki/INT_13H#INT_13h_AH=42h:_Extended_Read_Sectors_From_Drive
+    cmpw    $0x7f, %ax
+    ja      .Lerror
+
+    xorl    %ecx, %ecx
+    movw    %cx, %ds
+
+    subw    $0x10, %sp
+    movw    $0x10, (%esp)
+    movw    %ax, 2(%esp)
+    movl    %edx, 4(%esp)
+    movl    .Ldata_sector_num, %edx
+    movl    %edx, 8(%esp)
+    # 这里 %ecx 应该为0
+    movl    %ecx, 12(%esp)
+
+    addl    %eax, .Ldata_sector_num
+
+    movw    %ss, %dx
+    movw    %dx, %ds
+    movl    %esp, %esi
+    movb    $0x80, %dl
+    movb    $0x42, %ah
+    int     $0x13
+    jc      .Lerror
+    testb   %ah, %ah
+    jne     .Lerror
+    addw    $0x10, %sp
+    jmp     .Lclear
+
+.Lclear:
+    xorl    %eax, %eax
+    xorl    %ebx, %ebx
+    xorl    %ecx, %ecx
+    xorl    %edx, %edx
+    xorl    %esi, %esi
+    xorl    %edi, %edi
+    xorl    %ebp, %ebp
+    movw    %ax, %ds
+    movw    %ax, %es
+    movw    %ax, %fs
+    movw    %ax, %gs
+    cld
+    clc
+    ret
+
+1:
+    .ascii "error!"
+2:
+.Lerror:
+    movl    $0x1300, %eax
+    movl    $0b00001111, %ebx
+    movl    $(2b-1b), %ecx
+    xorl    %edx, %edx
+    movw    %dx, %es
+    movl    $1b, %ebp
+    int     $0x10
+    jmp     .
+
+
+    .fill 510-( . - _start ), 1, 0
+    .byte 0x55
+    .byte 0xaa
+
+.Lpart2:
+    //打开 A20
+    movw    $0x2401, %ax
+    int     $0x15
+    jc      .Lerror
+    testb   %ah, %ah
+    jnz     .Lerror
+    call    .Lclear
+
+    cli
+    lgdt    .Lgdt_ptr
+    sti
+
+    movl    0x8000, %eax
+    # 现在%eax存放了需要读取的扇区数
+
+    # while %eax > 0x7f
+2:
+    cmpl    $0x7f, %eax
+    jbe     1f
+    pushl   %eax
+    movw    $0x7f, %ax
+    movl    $0x30000000, %edx
+    call    .Lread_hdd
+    movl    $0xfe00, %eax
+    call    .Lcopy_to_high
+    popl    %eax
+    subl    $0x7f, %eax
+    jmp     2b
+
+1:
+    pushw   %ax
+    movl    $0x30000000, %edx
+    call    .Lread_hdd
+    popw    %ax
+    shll    $9, %eax
+    call    .Lcopy_to_high
+
+    # 初始化页表 0x10000~0x13fff 共 16kb
+
+    # 将 0x10000~0x12fff 12kb 内存 清0
+    movw    $0x1000, %cx
+    movw    %cx, %es
+    movw    $0xc00, %cx
+    rep     stosl
+
+    movl    $0x11003 , %eax
+    movl    %eax, %es:0x0
+
+    movl    $0x12003 , %eax
+    movl    %eax, %es:0x1000
+
+    movl    $0x13003 , %eax
+    movl    %eax, %es:0x2000
+
+    movw    $0x200, %cx
+    movl    $3, %eax
+1:
+    movl    %eax, %es:(%edi)
+    addw    $8, %di
+    addl    $0x1000, %eax
+    loopl   1b
+
+    # 重新加载gdt，删去不必要的段(16位和32位)
+    movw    $(.Lgdt64_end-.Lgdt_null-1), .Lgdt_ptr
+    cli
+    lgdt    .Lgdt_ptr
+
+    # 设置 %cr3
+    movl    $0x10000, %eax
+    movl    %eax, %cr3
+
+    # 设置 %cr4 的PAE位
+    movl    %cr4, %eax
+    orl     $( 1<<5 ), %eax
+    movl    %eax, %cr4
+
+    # 设置 EFER 寄存器的LME位
+    movl    $0xC0000080, %ecx
+    rdmsr
+    orl     $( 1 << 8 ), %eax
+    wrmsr
+
+    # 设置 %cr0 的 PE位 和 PG位
+    movl    %cr0, %eax
+    orl     $( (1 << 31) | (1 << 0) ), %eax
+    movl    %eax, %cr0
+
+    // 进入内核
+    ljmpl   $(.Lgdt_code64 - .Lgdt_null), $1f
+    .code64
+1:
+    movw    $(.Lgdt_data64 - .Lgdt_null), %ax
+    movw    %ax, %ds
+    movw    %ax, %es
+    movw    %ax, %fs
+    movw    %ax, %gs
+    movw    %ax, %ss
+    pushw   $(.Lgdt_code64 - .Lgdt_null)
+    pushq   $0x100000
+    lretq
+
+
+    .code16
+# 从 0x30000拷贝 %eax 个字节的数据到 *.Ldata_protected_mode_code
+.Lcopy_to_high:
+    pushl   %eax
+    call    .Lclear
+    cli
+    movl    %cr0, %eax
+    orl     $1, %eax
+    movl    %eax, %cr0
+    ljmpl   $(.Lgdt_code32-.Lgdt_null), $1f
+    .code32
+1:
+    movl    $(.Lgdt_data32-.Lgdt_null), %eax
+    movw    %ax, %ds
+    movw    %ax, %es
+    movw    %ax, %fs
+    movw    %ax, %gs
+    movw    %ax, %ss
+
+    popl    %ecx
+    movl    .Ldata_protected_mode_code, %edi
+    addl    %ecx, .Ldata_protected_mode_code
+    movb    %cl, %dl
+    addb    $0b11, %dl
+    shrl    $2, %ecx
+    movl    $0x30000, %esi
+    rep;    movsl
+    movb    %dl, %cl
+    rep;    movsb
+
+    // 返回实模式
+    movl    $(.Lgdt_data16-.Lgdt_null), %eax
+    movw    %ax, %ds
+    movw    %ax, %es
+    movw    %ax, %fs
+    movw    %ax, %gs
+    movw    %ax, %ss
+    ljmpl    $(.Lgdt_code16-.Lgdt_null), $1f
+    .code16
+1:
+    movl    %cr0, %eax
+    andl    $0xfffffffe, %eax
+    movl    %eax, %cr0
+    ljmpl   $0, $1f
+1:
     xorl    %eax, %eax
     movw    %ax, %fs
     movw    %ax, %gs
     movw    %ax, %ss
     movw    %ax, %ds
     movw    %ax, %es
-    movl    $0x7c00, %esp
     sti
-    call    .Lclear
+    jmp     .Lclear
---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CFLAGS_GLOBAL=("-std=c2x" "-g0" "-O3" "-Wall" "-Wextra" "-pedantic" "-fno-exceptions" "-fno-asynchronous-unwind-tables" "-fno-stack-check" "-fno-stack-clash-protection" "-fno-stack-protector" "-fno-unwind-tables" "-fcf-protection=none" "-fno-split-stack")
+
+check_dependency()
+{
+    local temp=('set' 'as' 'dd' 'ld' 'rm' 'qemu-img' 'echo' 'gcc')
+    for i in "${temp[@]}"
+    do
+        if ! command -V "$i" >/dev/null 2>&1; then
+            echo " \"${i}\" 命令未找到"
+            echo "请检查依赖是否正确安装"
+            exit 1
+        fi
+    done
+}
+check_dependency
+mkdir out 2>/dev/null
+set -e
+as --64 boot/bootloader.s -o out/bootloader.o
+ld --oformat binary -Ttext 0x7c00 -Tbss 0x0 -o out/bootloader.bin out/bootloader.o
+
+gcc "${CFLAGS_GLOBAL[@]}" -nostdinc -fpie -c kernel/main.c -o out/main.o
+ld -T build/build.ld -pie out/main.o -o out/kernel.bin
+
+gcc "${CFLAGS_GLOBAL[@]}" build/build_helper.c -o out/build_helper
+out/build_helper out/kernel.bin out/kernel_size
+
+dd conv=fdatasync if=out/bootloader.bin ibs=512 conv=sync of=out/boot.img
+dd conv=fdatasync if=out/kernel_size ibs=512 conv=sync of=out/boot.img oflag=append conv=notrunc
+dd conv=fdatasync if=out/kernel.bin ibs=512 conv=sync of=out/boot.img oflag=append conv=notrunc
+rm -rf out/boot.vmdk
+qemu-img convert -f raw -O vmdk out/boot.img out/boot.vmdk
